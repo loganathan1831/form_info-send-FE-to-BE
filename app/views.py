19fda94f31c2f80deffa6ea0f9b964d8319746a6
loganathan1831 <loga1831@gmail.com>
2023-01-29 12:07:46 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from app.models import *
+from django.http import HttpResponse
+# Create your views here.
+
+def insert_topic(request):
+    if request.method=="POST":
+        tn=request.POST['topic']
+        T=Topic.objects.get_or_create(topic_name=tn)[0]
+        T.save()
+        return HttpResponse('insert_topic is done')
+    
+    if request.method=='POST':
+        wp=request.POST['webpage']
+        W=Webpage.objects.get_or_create(Webpage=wp)[0]
+        W.save()
+        return HttpResponse('insert_topic is done')
+    
+    return render(request,'insert_topic.html')
